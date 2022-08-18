35cb238aed1d8491a7fcff18d1bff800cefac805
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 23:35:46 +0300

    commit tree 37e70e8f161e4fc70abae9648ce96e9fefd40bf3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
-
+#require "pry"
+#clas book
+class Book
+    attr_accessor :author, :page_count, :genre, :title
+    def initialize (title)
+       @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new "Adventist Home"
+puts book.title
